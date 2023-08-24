3a99b878de2f2d3f3501429e15a588315076ae45
Peter Hinge <contactpeterhinge@gmail.com>
2023-08-24 14:08:28 +0200

    proper template management

--- src/blog/views.py
+++ src/blog/views.py
@@ -4,8 +4,8 @@ from .models import Article
 
 def home(request):
     published_articles = Article.published_articles.all()
-    return render(request, 'home.html', {'articles' : published_articles})
+    return render(request, 'blog/home.html', {'articles' : published_articles})
 
 def single_article(request, article):
     article = get_object_or_404(Article, slug=article, status='published')   
-    return render(request, 'article.html', {'article' : article})
+    return render(request, 'blog/article.html', {'article' : article})
